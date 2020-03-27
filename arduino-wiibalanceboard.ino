8f90ae33531ad745f6d6c7230b9923ac2f660dfa
Ridvan San <sanridvan@gmail.com>
2020-03-27 16:36:10 +0300

    first commit

---
+++ arduino-wiibalanceboard.ino
@@ -0,0 +1,15 @@
+// This is a project for makaing a Wii Balance Board with Arduino.
+// I used 4 weight sensor for measurement and HC-06 Bluetooth module for communication.
+// You can upload this to your Arduino for using it.
+
+
+
+
+void setup(){
+
+}
+
+
+void loop(){
+
+} 
