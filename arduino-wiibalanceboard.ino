99db00b68b537eb1d965d102710d7e110fe349a1
Rıdvan San <sanridvan@gmail.com>
2020-10-24 00:11:13 +0300

    Added structs

--- arduino-wiibalanceboard.ino
+++ arduino-wiibalanceboard.ino
@@ -13,6 +13,7 @@ int llData;
 int lrData;
 
 
+// Might delete decimal and change to hexadecimal for easy imply
 void updateWeightData(int *upleft,int *upRight, int *lowLeft, int *lowRight){
     *upleft = digitalRead(UPPERLEFT);
     *upRight = digitalRead(UPPERRIGHT);
@@ -20,6 +21,34 @@ void updateWeightData(int *upleft,int *upRight, int *lowLeft, int *lowRight){
     *lowRight = digitalRead(LOWERRIGHT);
 }
 
+
+//Added data struct as it says in WiiBrew Wii Balance Board Page
+struct data {
+    uint16_t topRight;
+    uint16_t bottomRight;
+    uint16_t topLeft;
+    uint16_t bottomLeft;
+    uint8_t temperature;
+    uint8_t batteryLevel;
+};
+
+
+//Added calibration struct
+struct calibration{
+    uint16_t topRight0kg;
+    uint16_t bottomRight0kg;
+    uint16_t topLeft0kg;
+    uint16_t bottomLeft0kg;
+    uint16_t topRight17kg;
+    uint16_t bottomRight17kg;
+    uint16_t topLeft17kg;
+    uint16_t bottomLeft17kg;
+    uint16_t topRight34kg;
+    uint16_t bottomRight34kg;
+    uint16_t topLeft34kg;
+    uint16_t bottomLeft34kg;
+    uint32_t checksum;
+};
 
 
 
