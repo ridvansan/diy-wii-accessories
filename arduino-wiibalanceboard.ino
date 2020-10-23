2e31a829f2446cdb27b71551ffcf52a962b0f6af
Rıdvan San <sanridvan@gmail.com>
2020-10-23 13:59:14 +0300

    little fixes

--- arduino-wiibalanceboard.ino
+++ arduino-wiibalanceboard.ino
@@ -2,6 +2,25 @@
 // I used 4 weight sensor for measurement and HC-06 Bluetooth module for communication.
 // You can upload this to your Arduino for using it.
 
+#define UPPERLEFT 2
+#define UPPERRIGHT 3
+#define LOWERLEFT 4
+#define LOWERRIGHT 5
+
+int ulData;
+int urData;
+int llData;
+int lrData;
+
+
+void updateWeightData(int *upleft,int *upRight, int *lowLeft, int *lowRight){
+    *upleft = digitalRead(UPPERLEFT);
+    *upRight = digitalRead(UPPERRIGHT);
+    *lowLeft = digitalRead(LOWERLEFT);
+    *lowRight = digitalRead(LOWERRIGHT);
+}
+
+
 
 
 
